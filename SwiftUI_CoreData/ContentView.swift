4293b2623d089fc83b5effcece24fc90b1e16adc
Peter Delgado <peterrafaeldelgado@gmail.com>
2022-09-13 14:40:53 +0200

    Initial Commit

---
+++ SwiftUI_CoreData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_CoreData
+//
+//  Created by Peter Delgado on 13/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_CoreData/SwiftUI_CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CoreDataApp.swift
+//  SwiftUI_CoreData
+//
+//  Created by Peter Delgado on 13/9/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
